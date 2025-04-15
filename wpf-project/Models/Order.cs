2b41f76805a8cd8bec0e50fdc944018c3c5d9bea
huy023156 <huy023156@gmail.com>
2025-04-15 10:51:22 +0700

    Move orderItem to new file

--- wpf-project/Models/Order.cs
+++ wpf-project/Models/Order.cs
@@ -24,20 +24,4 @@ namespace wpf_project.Models
         public virtual User User { get; set; }
         public virtual ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
     }
-
-    public class OrderItem
-    {
-        public int OrderId { get; set; }
-        
-        public int BookId { get; set; }
-        
-        public int Quantity { get; set; }
-        
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal UnitPrice { get; set; }
-        
-        // Navigation properties
-        public virtual Order Order { get; set; }
-        public virtual Book Book { get; set; }
-    }
 }
---
+++ wpf-project/Models/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_project.Models
+{
+    public class OrderItem 
+    {
+        public int OrderId { get; set; }
+
+        public int BookId { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+
+        // Navigation properties
+        public virtual Order Order { get; set; }
+        public virtual Book Book { get; set; }
+    }
+}
